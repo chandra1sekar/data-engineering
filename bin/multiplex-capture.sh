d2deb2e023e6f29a42ec60abcec4e1595473e389
Mark Mims <mark.mims@ubuntu.com>
2017-02-17 16:09:08 -0800

    updates

--- bin/multiplex-capture.sh
+++ bin/multiplex-capture.sh
@@ -13,7 +13,7 @@ generate_test_pattern() {
   local duration=$1
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
-    -f lavfi -i testsrc=duration=${duration}:size=hd1080:rate=30:decimals=2 \
+    -f lavfi -i testsrc=duration=${duration}:size=hd1080:rate=${FR}:decimals=2 \
     testpattern.mkv > testpattern.log 2>&1
 }
 
@@ -24,18 +24,7 @@ echo "generating test pattern for $duration seconds"
 time generate_test_pattern $duration
 echo "testpattern done"
 
-
 echo "building composite"
-
-#ffmpeg \
-	#-i webcam.mkv \
-	#-i slides.mkv \
-	#-i terminal.mkv \
-	#-i testpattern.mkv \
-	#-filter_complex "nullsrc=size=1920x1080 [base]; [0:v] setpts=PTS-STARTPTS, scale=960x540 [upperleft]; [1:v] setpts=PTS-STARTPTS, scale=960x540 [upperright]; [2:v] setpts=PTS-STARTPTS, scale=960x540 [lowerleft]; [3:v] setpts=PTS-STARTPTS, scale=960x540 [lowerright]; [base][upperleft] overlay=shortest=1 [tmp1]; [tmp1][upperright] overlay=shortest=1:x=960 [tmp2]; [tmp2][lowerleft] overlay=shortest=1:y=540 [tmp3]; [tmp3][lowerright] overlay=shortest=1:x=960:y=540 " \
-	#-c:v libx264 output.mkv
-
-#combine_filter="nullsrc=size=1920x1080 [base]; [0:v] setpts=PTS-STARTPTS, scale=960x540 [upperleft]; [1:v] setpts=PTS-STARTPTS, scale=960x540 [upperright]; [2:v] setpts=PTS-STARTPTS, scale=960x540 [lowerleft]; [3:v] setpts=PTS-STARTPTS, scale=960x540 [lowerright]; [base][upperleft] overlay=shortest=1 [tmp1]; [tmp1][upperright] overlay=shortest=1:x=960 [tmp2]; [tmp2][lowerleft] overlay=shortest=1:y=540 [tmp3]; [tmp3][lowerright] overlay=shortest=1:x=960:y=540 " \
 
 read -r -d '' combine_filter <<'EOS'
   nullsrc=size=1920x1080 [base];
--- bin/start-capture.sh
+++ bin/start-capture.sh
@@ -3,55 +3,41 @@
 exit 1
 
 FR=24
+OUTPUT_DIR="capture-$TS"
 
-#Ok here are the orig commands
-#ffmpeg -f x11grab -r 15 -s 1920x1080 -i :0.0+1920,0 -vcodec libx264 -preset ultrafast slides.mkv
-#ffmpeg -f x11grab -r 15 -s 1920x1080 -i :0.0+0,0 -vcodec libx264 -preset ultrafast terminal.mkv
-#ffmpeg -f pulse -i default -f v4l2 -framerate 30 -input_format h264 -video_size hd1080 -i /dev/video0 -preset ultrafast -c copy talking_head.mkv
+##################
 
 capture_slides() {
   local output_dir=$1
-  local duration=$2
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
     -f x11grab -r ${FR} -s hd1080 -i :0.0+1920,0 \
     -vcodec libx264 \
     -preset ultrafast \
     $output_dir/slides.mkv > $output_dir/slides.log 2>&1
-  echo "slides done"
 }
 
 capture_terminal() {
   local output_dir=$1
-  local duration=$2
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
     -f x11grab -r ${FR} -s hd1080 -i :0.0+0,0 \
     -vcodec libx264 \
     -preset ultrafast \
     $output_dir/terminal.mkv > $output_dir/terminal.log 2>&1
-  echo "terminal done"
 }
 
 capture_webcam() {
   local output_dir=$1
-  local duration=$2
-    #-preset ultrafast \
-    #-f pulse -i default \
-    #-f alsa -i pulse \
-    #-r 15 \
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
     -f pulse -i default \
     -f v4l2 -framerate ${FR} -input_format h264 -video_size hd1080 -i /dev/video0 \
     -c copy \
     $output_dir/webcam.mkv > $output_dir/webcam.log 2>&1
-  echo "webcam done"
 }
 
 TS=`date +%Y%m%d%H%M`
-
-OUTPUT_DIR="capture-$TS"
 
 mkdir -p $OUTPUT_DIR
 
@@ -59,12 +45,15 @@ echo "starting run at $TS"
 
 echo "capturing slides"
 capture_slides $OUTPUT_DIR &
+echo "slides done"
 
 echo "capturing terminal"
 capture_terminal $OUTPUT_DIR &
+echo "terminal done"
 
 echo "capturing webcam"
 capture_webcam $OUTPUT_DIR &
+echo "webcam done"
 
 for job in `jobs -p`; do
   wait $job
