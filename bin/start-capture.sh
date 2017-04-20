972d7c905c9f120faa761ad0c88d683c7aed9aa3
Mark Mims <mark.mims@ubuntu.com>
2017-04-20 14:58:16 -0700

    audio offset

--- bin/start-capture.sh
+++ bin/start-capture.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-TS=`date +%Y-%m-%d-%H%M`
-OUTPUT_DIR="capture-$TS"
-FR=30
+set -o errexit -o nounset -o pipefail
 
-##################
+usage() {
+	echo "Usage: $0 <section_number>
+    where <section_number> is something like 1.1.2"
+}
+(( $# != 1 )) && usage && exit 1
+section=$1
+
+timestamp=`date +%Y-%m-%d-%H%M`
+output_dir="captures/${section}/capture-${timestamp}"
+framerate=30
 
 capture_slides() {
   local output_dir=$1
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
-    -f x11grab -r ${FR} -s hd1080 -i :0.0+1920,0 \
+    -f x11grab -r $framerate -s hd1080 -i :0.0+1920,0 \
     -vcodec libx264 \
     -preset ultrafast \
     $output_dir/slides.mkv > $output_dir/slides.log 2>&1
@@ -21,7 +28,7 @@ capture_terminal() {
   local output_dir=$1
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
-    -f x11grab -r ${FR} -s hd1080 -i :0.0+0,0 \
+    -f x11grab -r $framerate -s hd1080 -i :0.0+0,0 \
     -vcodec libx264 \
     -preset ultrafast \
     $output_dir/terminal.mkv > $output_dir/terminal.log 2>&1
@@ -32,25 +39,39 @@ capture_webcam() {
   local output_dir=$1
   ffmpeg \
     -hide_banner -nostats -loglevel warning \
-    -f pulse -i default \
-    -f v4l2 -framerate ${FR} -input_format h264 -video_size hd1080 -i /dev/video0 \
+    -f pulse  -itsoffset -0.2 -i default \
+    -f v4l2 -framerate $framerate -input_format h264 -video_size hd1080 -i /dev/video0 \
     -c copy \
     $output_dir/webcam.mkv > $output_dir/webcam.log 2>&1
   echo "webcam done"
 }
 
-echo "starting run at $TS"
 
-mkdir -p $OUTPUT_DIR
+##################
+
+# Get arguments
+#while getopts ':s:' OPT; do
+#  case $OPT in
+#    s) section=$OPTARG ;;
+#    e) env=$OPTARG ;;
+#    l) layer=$OPTARG ;;
+#    n) nocolour='-no-color' ;;
+#    x) xtraopts=$OPTARG ;;
+#  esac
+#done
+
+echo "starting run for $section at $timestamp"
+
+mkdir -p $output_dir
 
 echo "capturing slides"
-capture_slides $OUTPUT_DIR &
+capture_slides $output_dir &
 
 echo "capturing terminal"
-capture_terminal $OUTPUT_DIR &
+capture_terminal $output_dir &
 
 echo "capturing webcam"
-capture_webcam $OUTPUT_DIR &
+capture_webcam $output_dir &
 
 for job in `jobs -p`; do
   wait $job
