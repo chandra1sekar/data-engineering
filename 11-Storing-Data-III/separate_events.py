0d85bdff13c75092dafa9bcf84094abfc054eefd
Mark Mims <mark.mims@ubuntu.com>
2018-03-19 09:32:37 -0700

    more deps

---
+++ 11-Storing-Data-III/separate_events.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import udf
+
+
+@udf('string')
+def munge_event(event_as_json):
+    event = json.loads(event_as_json)
+    event['Host'] = "moe"
+    return json.dumps(event)
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .getOrCreate()
+
+    raw_events = spark \
+        .read \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .option("startingOffsets", "earliest") \
+        .option("endingOffsets", "latest") \
+        .load()
+
+    munged_events = raw_events \
+        .select(raw_events.value.cast('string').alias('raw'),
+                raw_events.timestamp.cast('string')) \
+        .withColumn('munged', munge_event('raw'))
+
+    extracted_events = munged_events \
+        .rdd \
+        .map(lambda r: Row(timestamp=r.timestamp, **json.loads(r.munged))) \
+        .toDF()
+
+    sword_purchases = extracted_events \
+        .filter(extracted_events.event_type == 'purchase_sword')
+    sword_purchases.show()
+    # sword_purchases \
+        # .write \
+        # .mode("overwrite") \
+        # .parquet("/tmp/sword_purchases")
+
+    default_hits = extracted_events \
+        .filter(extracted_events.event_type == 'default')
+    default_hits.show()
+    # default_hits \
+        # .write \
+        # .mode("overwrite") \
+        # .parquet("/tmp/default_hits")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 11-Storing-Data-III/transform_events.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import udf
+
+
+@udf('string')
+def munge_event(event_as_json):
+    event = json.loads(event_as_json)
+    event['Host'] = "moe"
+    return json.dumps(event)
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .getOrCreate()
+
+    raw_events = spark \
+        .read \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .option("startingOffsets", "earliest") \
+        .option("endingOffsets", "latest") \
+        .load()
+
+    munged_events = raw_events \
+        .select(raw_events.value.cast('string').alias('raw'),
+                raw_events.timestamp.cast('string')) \
+        .withColumn('munged', munge_event('raw'))
+    munged_events.show()
+
+    extracted_events = munged_events \
+        .rdd \
+        .map(lambda r: Row(timestamp=r.timestamp, **json.loads(r.munged))) \
+        .toDF()
+    extracted_events.show()
+
+    extracted_events \
+        .write \
+        .mode("overwrite") \
+        .parquet("/tmp/extracted_events")
+
+
+if __name__ == "__main__":
+    main()
