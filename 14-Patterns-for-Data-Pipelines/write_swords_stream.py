f8184abc6e82a1ba342b9b402b895443864edc10
Mark Mims <mark.mims@ubuntu.com>
2018-04-16 08:08:40 -0600

    files for wk14 sync

---
+++ 14-Patterns-for-Data-Pipelines/write_swords_stream.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, from_json
+from pyspark.sql.types import StructType, StructField, StringType
+
+
+def purchase_sword_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    |-- timestamp: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+
+@udf('boolean')
+def is_sword_purchase(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'purchase_sword':
+        return True
+    return False
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .getOrCreate()
+
+    raw_events = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .load()
+
+    sword_purchases = raw_events \
+        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          purchase_sword_event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+    sink = sword_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_sword_purchases") \
+        .option("path", "/tmp/sword_purchases") \
+        .trigger(processingTime="120 seconds") \
+        .start()
+
+    sink.awaitTermination()
+
+
+if __name__ == "__main__":
+    main()
